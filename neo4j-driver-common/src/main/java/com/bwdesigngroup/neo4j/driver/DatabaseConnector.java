3da63f7ee3830efbfe310eac8cb65604170c8da2
Keith Gamble <keith.gamble@bwdesigngroup>
2021-03-16 15:16:32 -0700

    Added DatabaseConnector Class based off helloWorld

---
+++ neo4j-driver-common/src/main/java/com/bwdesigngroup/neo4j/driver/DatabaseConnector.java
@@ -0,0 +1,56 @@
+package com.bwdesigngroup.neo4j.driver;
+
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Transaction;
+import org.neo4j.driver.TransactionWork;
+
+import static org.neo4j.driver.Values.parameters;
+
+
+public class DatabaseConnector implements AutoCloseable
+{
+    private final Driver driver;
+
+    public DatabaseConnector( String uri, String user, String password )
+    {
+        driver = GraphDatabase.driver( uri, AuthTokens.basic( user, password ) );
+    }
+
+    @Override
+    public void close() throws Exception
+    {
+        driver.close();
+    }
+
+    public void printGreeting( final String message )
+    {
+        try ( Session session = driver.session() )
+        {
+            String greeting = session.writeTransaction( new TransactionWork<String>()
+            {
+                @Override
+                public String execute( Transaction tx )
+                {
+                    Result result = tx.run( "CREATE (a:Greeting) " +
+                                                     "SET a.message = $message " +
+                                                     "RETURN a.message + ', from node ' + id(a)",
+                            parameters( "message", message ) );
+                    return result.single().get( 0 ).asString();
+                }
+            } );
+            System.out.println( greeting );
+        }
+    }
+
+    public static void main( String... args ) throws Exception
+    {
+        try ( DatabaseConnector greeter = new DatabaseConnector( "bolt://localhost:7687", "neo4j", "password" ) )
+        {
+            greeter.printGreeting( "hello, world" );
+        }
+    }
+}
