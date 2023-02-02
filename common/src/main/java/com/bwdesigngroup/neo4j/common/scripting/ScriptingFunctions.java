b40a3f650c957c35fe7b3a6838af0b85c21fb77e
keith-gamble <keith.gamble@bwdesigngroup.com>
2023-02-01 21:25:29 -0800

    Updated common paths

--- common/src/main/java/com/bwdesigngroup/neo4j/common/scripting/ScriptingFunctions.java
+++ common/src/main/java/com/bwdesigngroup/neo4j/common/scripting/ScriptingFunctions.java
@@ -2,8 +2,6 @@ package com.bwdesigngroup.neo4j.common.scripting;
 
 import java.util.List;
 
-import com.inductiveautomation.ignition.common.script.hints.NoHint;
-
 import org.python.core.PyObject;
 
 public interface ScriptingFunctions {
