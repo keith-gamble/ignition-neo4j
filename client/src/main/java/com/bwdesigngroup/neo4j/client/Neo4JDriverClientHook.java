4fbcd31126dfd033ee2eab03f079b15c94fccbc9
keith-gamble <keith.gamble@bwdesigngroup.com>
2023-02-01 21:25:56 -0800

    Removed unused imports in client

--- client/src/main/java/com/bwdesigngroup/neo4j/client/Neo4JDriverClientHook.java
+++ client/src/main/java/com/bwdesigngroup/neo4j/client/Neo4JDriverClientHook.java
@@ -1,12 +1,7 @@
 package com.bwdesigngroup.neo4j.client;
 
-import com.bwdesigngroup.neo4j.common.scripting.ScriptModule;
-import com.bwdesigngroup.neo4j.common.scripting.ScriptingFunctions;
-
-import com.inductiveautomation.ignition.client.gateway_interface.ModuleRPCFactory;
 import com.inductiveautomation.ignition.client.model.ClientContext;
 import com.inductiveautomation.ignition.common.licensing.LicenseState;
-import com.inductiveautomation.ignition.common.script.hints.NoHint;
 import com.inductiveautomation.ignition.common.script.ScriptManager;
 import com.inductiveautomation.ignition.common.script.hints.PropertiesFileDocProvider;
 import com.inductiveautomation.vision.api.client.AbstractClientModuleHook;
